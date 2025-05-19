4c06103d2b0db9897766c799eb2b26e65ab848bd
Nazar Bakay <bakayn484@gmail.com>
2025-05-19 23:22:01 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  const largeImg = document.getElementById('largeImg');
+
+  largeImg.src = link.href;
+});
