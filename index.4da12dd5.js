69574b219d069d53da145f7416eae531e66b3b0d
Nazar Bakay <bakayn484@gmail.com>
2025-05-19 23:52:18 +0300

    make build

---
+++ index.4da12dd5.js
@@ -0,0 +1,2 @@
+document.querySelector("#thumbs").addEventListener("click",function(e){var t=e.target.closest(".list-item__link");t&&(e.preventDefault(),document.getElementById("largeImg").src=t.href)});
+//# sourceMappingURL=index.4da12dd5.js.map
